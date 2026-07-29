c951eebd57fe7aaf6435edfdbae48db96cb092e5
emter80 <emter80@gmail.com>
2026-07-29 10:58:25 +0200

    Add Headlamp platform resources

--- multipass/k3s/02-bootstrap/05-gitops/applicationset.tf
+++ multipass/k3s/02-bootstrap/05-gitops/applicationset.tf
@@ -2,66 +2,87 @@ resource "kubernetes_manifest" "platform_applicationset" {
   manifest = {
     apiVersion = "argoproj.io/v1alpha1"
     kind       = "ApplicationSet"
+
     metadata = {
       name      = "platform-apps"
       namespace = "argocd"
     }
+
     spec = {
       goTemplate = true
+
       goTemplateOptions = [
         "missingkey=error"
       ]
+
       generators = [
         {
           git = {
-            repoURL = "https://github.com/emter80/kubernetes-mate-lab.git"
+            repoURL  = "https://github.com/emter80/kubernetes-mate-lab.git"
             revision = "main"
+
             files = [
               {
                 path = "multipass/k3s/03-apps/applications/**/app.yaml"
+              },
+              {
+                path = "multipass/k3s/03-apps/platform/**/app.yaml"
               }
             ]
           }
         }
       ]
+
       template = {
         metadata = {
           name = "{{.name}}"
         }
+
         spec = {
           project = "default"
-          sources = [
-            {
-              repoURL = "{{.repoURL}}"
-              chart = "{{.chart}}"
-              targetRevision = "{{.targetRevision}}"
-              helm = {
-                valueFiles = [
-                  "$values/{{.valuesFile}}"
-                ]
-              }
-            },
-            {
-              repoURL = "https://github.com/emter80/kubernetes-mate-lab.git"
-              targetRevision = "main"
-              ref = "values"
-            }
-          ]
+
           destination = {
             server = "https://kubernetes.default.svc"
             namespace = "{{.namespace}}"
           }
+
           syncPolicy = {
             automated = {
-              prune = true
+              prune    = true
               selfHeal = true
             }
+
             syncOptions = [
               "CreateNamespace=true"
             ]
           }
         }
       }
+
+      templatePatch = <<-EOT
+        spec:
+          sources:
+          {{- if .chart }}
+
+          - repoURL: {{.repoURL}}
+            chart: {{.chart}}
+            targetRevision: {{.targetRevision}}
+            helm:
+              valueFiles:
+                - $values/{{.valuesFile}}
+
+          - repoURL: https://github.com/emter80/kubernetes-mate-lab.git
+            targetRevision: main
+            ref: values
+
+          {{- else }}
+
+          - repoURL: {{.repoURL}}
+            targetRevision: {{.targetRevision}}
+            path: {{.path}}
+
+          {{- end }}
+      EOT
     }
   }
 }
