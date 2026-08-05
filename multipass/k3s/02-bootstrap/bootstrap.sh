324857000eab697a62964f752b5e4819e46eb712
Mariusz Terlecki <157527106+emter80@users.noreply.github.com>
2026-08-05 11:51:32 +0200

    Modify directory list in bootstrap.sh
    Updated the directory list in bootstrap.sh to include sealed-secrets and reorder existing entries.

--- multipass/k3s/02-bootstrap/bootstrap.sh
+++ multipass/k3s/02-bootstrap/bootstrap.sh
@@ -3,15 +3,15 @@
 set -e
 
 DIRS=(
-  #"00-coredns"
   "01-cert-manager"
   "02-certificates"
-  "03-argocd"
-  "04-ingress"
-  "05-gitops"
-  "06-secrets"
-  "07-trust-manager"
-  "08-oidc"
+  "03-sealed-secrets"
+  "04-argocd"
+  "05-ingress"
+  "06-gitops"
+  "07-secrets"
+  "08-trust-manager"
+  "09-oidc"
 )
 
 for dir in "${DIRS[@]}"; do
