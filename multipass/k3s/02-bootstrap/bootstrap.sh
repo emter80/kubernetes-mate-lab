cf5bf3c7376706681ea788ca7852a2b44222e880
emter80 <emter80@gmail.com>
2026-08-06 11:34:33 +0200

    modify bootstrap

--- multipass/k3s/02-bootstrap/bootstrap.sh
+++ multipass/k3s/02-bootstrap/bootstrap.sh
@@ -35,23 +35,5 @@ for dir in "${DIRS[@]}"; do
     echo "================================="
 
     terraform apply -auto-approve
-
     cd ..
-
-    if [ "$dir" == "00-coredns" ]; then
-
-        echo "================================="
-        echo "Restarting CoreDNS"
-        echo "================================="
-
-        kubectl rollout restart deployment coredns -n kube-system
-
-        echo "================================="
-        echo "Waiting for CoreDNS rollout"
-        echo "================================="
-
-        kubectl rollout status deployment coredns -n kube-system --timeout=120s
-
-    fi
-
 done
